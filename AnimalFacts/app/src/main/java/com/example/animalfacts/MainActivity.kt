c676085f28c6e705db78812745a38b643e56e229
Varga Kristóf László <varga.kristof.laszlo@gmail.com>
2023-04-19 16:45:11 +0200

    using navgraph

--- AnimalFacts/app/src/main/java/com/example/animalfacts/MainActivity.kt
+++ AnimalFacts/app/src/main/java/com/example/animalfacts/MainActivity.kt
@@ -10,34 +10,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.animalfacts.navigation.NavGraph
 import com.example.animalfacts.ui.theme.AnimalFactsTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AnimalFactsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            AnimalFactsTheme() {
+                NavGraph()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AnimalFactsTheme {
-        Greeting("Android")
     }
 }
