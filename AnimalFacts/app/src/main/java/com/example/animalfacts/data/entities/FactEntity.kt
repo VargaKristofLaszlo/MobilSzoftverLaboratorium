dbdecf7a4a3d2bc2dc70694210ef5d4d0c300bc9
Varga Kristóf László <varga.kristof.laszlo@gmail.com>
2023-04-30 11:30:00 +0200

    column infos

--- AnimalFacts/app/src/main/java/com/example/animalfacts/data/entities/FactEntity.kt
+++ AnimalFacts/app/src/main/java/com/example/animalfacts/data/entities/FactEntity.kt
@@ -1,5 +1,6 @@
 package com.example.animalfacts.data.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.*
@@ -7,10 +8,10 @@ import java.util.*
 @Entity(tableName = "fact_table")
 data class FactEntity(
     @PrimaryKey(autoGenerate = false) val _id: String,
-    val __v: Int,
-    val text: String,
-    val updatedAt: Date,
-    val deleted: Boolean,
-    val source: String,
-    val sentCount: Int
+    @ColumnInfo(name = "__v") val __v: Int,
+    @ColumnInfo(name = "Text") val text: String,
+    @ColumnInfo(name = "Update_at") val updatedAt: Date,
+    @ColumnInfo(name = "Deleted") val deleted: Boolean,
+    @ColumnInfo(name = "Source") val source: String,
+    @ColumnInfo(name = "SentCount") val sentCount: Int
 )
