e710ea55b506db9b59f1be7de24d40cc91f647e9
Varga Kristóf László <varga.kristof.laszlo@gmail.com>
2023-05-03 14:50:25 +0200

    FactService interface + retrofit gradle

--- AnimalFacts/app/src/main/java/com/example/animalfacts/network/FactService.kt
+++ AnimalFacts/app/src/main/java/com/example/animalfacts/network/FactService.kt
@@ -4,12 +4,13 @@ import com.example.animalfacts.domain.model.Fact
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class FactService @Inject constructor() {
-    fun getFacts(animal_type: String, amount: Int): Flow<List<Fact>> {
+class FactServiceImpl @Inject constructor(): FactService {
+    override fun getFacts(animal_type: String, amount: Int): Flow<List<Fact>> {
         TODO("Not yet implemented")
     }
 
-    fun getFactById(id: String): Flow<Fact> {
+    override fun getFactById(id: String): Flow<Fact> {
         TODO("Not yet implemented")
     }
+
 }
